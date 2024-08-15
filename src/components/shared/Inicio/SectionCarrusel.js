80f527dbfe489e83a462b439d7a6c613c1e655ab
LAPTOP-L7H0H0EL\glez_ <Jesus_Glez>
2024-08-15 14:27:54 -0600

    Version funcionando carrusel y  las hojas de typo de evento con icono en color en responsivo

--- src/components/shared/Inicio/SectionCarrusel.js
+++ src/components/shared/Inicio/SectionCarrusel.js
@@ -35,7 +35,7 @@ export const SectionCarrusel = ({ title, type, items, itemType }) => {
 
     const settings = {
         dots: false,
-        infinite: false,
+        infinite: true,
         speed: 500,
         slidesToShow: 3,
         slidesToScroll: 1,
